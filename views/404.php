3d4fd3259f992ef45697dcd89e6f94d2b4bf8009
Mohamad Al-khatib <alkhatib.m804@gmail.com>
2021-11-01 17:38:23 +0100

    implementer pdosingleton

---
+++ views/404.php
@@ -0,0 +1,15 @@
+<?php require_once 'template/header.php' ?>
+<title>404 error</title>
+</head>
+
+<body>
+
+    <?php require_once 'template/nav.php' ?>
+    <main>
+        <h1>Page non trouvée</h1>
+    </main>
+    <?php require_once 'template/footer.php' ?>
+
+</body>
+
+</html>
---
+++ views/template/footer.php
@@ -0,0 +1,38 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="fisrt-col col-lg-3 col-md-6 col-sm-12">
+                <a href="#"><img src="style/images/logo-white.svg" alt="" /></a>
+                <div class="icons">
+                    <a href="#"><img src="<?= "$domain$views"?>style/images/icon-facebook.svg"
+                            alt="icon-facebook" /></a>
+                    <a href="#"><img src="<?= "$domain$views"?>style/images/icon-youtube.svg" alt="icon-youtube" /></a>
+                    <a href="#"><img src="<?= "$domain$views"?>style/images/icon-twitter.svg" alt="icon-twitter" /></a>
+                    <a href="#"><img src="<?= "$domain$views"?>style/images/icon-pinterest.svg"
+                            alt="icon-pinterest" /></a>
+                    <a href="#"><img src="<?= "$domain$views"?>style/images/icon-instagram.svg"
+                            alt="icon-instagram" /></a>
+                </div>
+            </div>
+            <div class="sec-col col-lg-3 col-md-6 col-sm-12">
+                <ul>
+                    <li><a href="#">About Us</a></li>
+                    <li><a href="#">Contact</a></li>
+                    <li><a href="#">Blog</a></li>
+                </ul>
+            </div>
+            <div class="third-col col-lg-3 col-md-6 col-sm-12">
+                <ul>
+                    <li><a href="#">Careers</a></li>
+                    <li><a href="#">Support</a></li>
+                    <li><a href="#">Privacy Policy</a></li>
+                </ul>
+            </div>
+            <div class="forth-col col-lg-3 col-md-6 col-sm-12">
+                <p>© Easybank. All Rights Reserved</p>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<script src="script/sandbox.js"></script>
